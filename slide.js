ae49f94893cfe55095408652c18398053ea3d4eb
Hirenkumar Patel <hirenkumar@me.com>
2017-09-02 14:40:12 +1000

    Add files via upload

---
+++ slide.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+
+//window.onload = downloadingImage();
+
+
+
+
+downloadingImage = function (){
+	
+	var my_image = new Image();
+    my_image.src = "image/11.png";
+	document.getElementById('imageSlider').src = "image/11.png";
+	
+	
+}
