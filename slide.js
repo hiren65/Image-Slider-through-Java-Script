cd8595ec7bdcc9ded1c6494b71b5a6ed356adcb0
Hirenkumar Patel <hirenkumar@me.com>
2017-09-03 00:16:19 +1000

    Add files via upload

--- slide.js
+++ slide.js
@@ -3,13 +3,28 @@
 //window.onload = downloadingImage();
 
 
-
+var allImageArray = ["image/11.png","image/12.png","image/13.png","image/14.png","image/15.png","image/16.png","image/17.png","image/18.png","image/19.png","image/20.png"];
+var count = 0;
 
 downloadingImage = function (){
 	
-	var my_image = new Image();
-    my_image.src = "image/11.png";
-	document.getElementById('imageSlider').src = "image/11.png";
+	//var my_image = new Image();
+    //my_image.src = "image/11.png";
+	
+	if(count>9){
+		count = 0;
+	 document.getElementById('imageSlider').src = allImageArray[count];	
+		count++;
+	}else
+	
+	if(count<10){
+		
+		   document.getElementById('imageSlider').src = allImageArray[count];
+		   count++;
+	}
+	
+	
+	//document.getElementById('imageSlider').src = "image/11.png";
 	
 	
 }
